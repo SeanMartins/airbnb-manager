b5768e6efe078b1762c2b738c45f36ed7b06974d
SeanMartins <martinsosem@gmail.com>
2026-06-12 11:22:53 +0200

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-v3bmaiAl_QbqxVyqYerOBC76TxEm3uw",
+  authDomain: "airbnb-firenze-f45e9.firebaseapp.com",
+  projectId: "airbnb-firenze-f45e9",
+  storageBucket: "airbnb-firenze-f45e9.firebasestorage.app",
+  messagingSenderId: "563836044492",
+  appId: "1:563836044492:web:b1f28c6302c445bf80ae69"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
